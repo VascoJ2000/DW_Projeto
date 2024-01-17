b46b383784e497b63d35a8b41e50f926edce4cef
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-01-17 11:05:51 +0000

    Update queries

--- src/nights/queries.js
+++ src/nights/queries.js
@@ -1,19 +1,22 @@
-const getNightByUserID = "SELECT * FROM movieNights WHERE user_id = $1";
-const getNightByID = "SELECT * FROM movieNights WHERE movieNight_id = $1";
-const addNight = "INSERT INTO movieNights (user_id, movie_id, movieNight_date, description) VALUES ($1, $2, $3, $4)";
-const removeNights = "DELETE FROM movieNights WHERE movieNights_id = $1";
-const removeNightsUser = "DELETE FROM movieNights WHERE user_id = $1";
-const updateMovie = "UPDATE movieNights SET movie_id = $1 WHERE movieNights_id = $2";
-const updateDate = "UPDATE movieNights SET movieNights_date = $1 WHERE movieNights_id = $2";
-const updateDescription = "UPDATE movieNights SET description = $1 WHERE movieNights_id = $2";
+// movie_night table queries
+const getNightByID = "SELECT * FROM movie_nights WHERE movie_night_id = $1";
+const addNight = "INSERT INTO movie_nights (movie_id, movie_night_date, description) VALUES ($1, $2, $3)";
+const removeNights = "DELETE FROM movie_nights WHERE movie_night_id = $1";
+const updateMovie = "UPDATE movie_nights SET movie_id = $1 WHERE movie_night_id = $2";
+const updateDate = "UPDATE movie_nights SET movie_night_date = $1 WHERE movie_night_id = $2";
+const updateDescription = "UPDATE movie_nights SET description = $1 WHERE movie_night_id = $2";
+
+// user_night table queries
+const getNightByUserID = "SELECT user_nights.user_id, user_nights.movie_night_id, movie_id, movie_night_date, user_nights.confirmed, description FROM user_nights INNER JOIN movie_nights ON movie_nights.movie_night_id = user_nights.movie_night_id WHERE (user_id, user_nights.movie_night_id) = ($1, $2)";
+const removeNightsUser = "DELETE FROM user_nights WHERE user_id = $1";
 
 module.exports = {
-    getNightByUserID,
     getNightByID,
     addNight,
     removeNights,
-    removeNightsUser,
     updateMovie,
     updateDate,
     updateDescription,
+    getNightByUserID,
+    removeNightsUser,
 };
