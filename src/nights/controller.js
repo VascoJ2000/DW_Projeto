842930490f09ad08f1b0920e81b48a86117f79b4
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-01-17 12:01:22 +0000

    update nights

--- src/nights/controller.js
+++ src/nights/controller.js
@@ -3,6 +3,7 @@ const queries = require('./queries');
 
 const getNightByUserID = (req, res) => {
     const id = parseInt(req.token.user_id);
+
     pool.query(queries.getNightByUserID, [id], (err, data) => {
         if(err) return res.sendStatus(400);
         res.status(200).json(data.rows);
@@ -11,20 +12,40 @@ const getNightByUserID = (req, res) => {
 
 const getNightByID = (req, res) => {
     const id = parseInt(req.params.id);
+
+    pool.query(queries.getUserNight, [req.token.user_id, id], (err, data) => {
+        if(err) return res.sendStatus(400);
+        if(!data.rows.length) return res.sendStatus(403);
+    });
+
     pool.query(queries.getNightByID, [id], (err, data) => {
         if(err) return res.sendStatus(400);
         if(!data.rows.length) return res.sendStatus(404);
-        if(!(data.rows[0].user_id === req.token.user_id)) return res.sendStatus(403);
         res.status(200).json(data.rows[0]);
     });
 }
 
 const addMovieNight = (req, res) => {
-    const movie_id = parseFloat(req.body.movie_id);
+    const id = parseInt(req.token.user_id);
+    const movie_id = parseInt(req.body.movie_id);
     const night_date  = Date.parse(req.body.date) || new Date();
     const description = req.body.description || null;
     
     pool.query(queries.addNight, [movie_id, night_date, description], (err, data) => {
+        if(err) return res.sendStatus(400);
+
+        pool.query(queries.addUserNight, [id, movie_id, true],(err, data) => {
+            if(err) return res.sendStatus(400);
+            res.status(201).json("Movie Night created Successfully");
+        });
+    });
+}
+
+const addUserToNight = (req, res) => {
+    const id = parseInt(req.body.user_id);
+    const movie_id = parseInt(req.body.movie_id);
+
+    pool.query(queries.addUserNight, [id, movie_id, false],(err, data) => {
         if(err) return res.sendStatus(400);
         res.status(201).json("Movie Night created Successfully");
     });
@@ -33,9 +54,13 @@ const addMovieNight = (req, res) => {
 const removeMovieNight = (req, res) => {
     const id = parseInt(req.params.id);
 
+    pool.query(queries.getUserNight, [req.token.user_id, id], (err, data) => {
+        if(err) return res.sendStatus(400);
+        if(!data.rows.length) return res.sendStatus(403);
+    });
+
     pool.query(queries.getNightByID, [id], (err, data) => {
         if(!data.rows.length) return res.status(404).send("Movie Night does not exist in the database!");
-        if(!(data.rows[0].user_id === req.token.user_id)) return res.sendStatus(403);
         
         pool.query(queries.removeNights, [id], (err, data) => {
             if(err) return res.sendStatus(400);
@@ -47,9 +72,13 @@ const removeMovieNight = (req, res) => {
 const updateMovieNight = (req, res) => {
     const id = parseInt(req.params.id);
 
+    pool.query(queries.getUserNight, [req.token.user_id, id], (err, data) => {
+        if(err) return res.sendStatus(400);
+        if(!data.rows.length) return res.sendStatus(403);
+    });
+
     pool.query(queries.getNightByID, [id], (err, data) => {
         if(!data.rows.length) return res.send("Movie Night does not exist in the database!");
-        if(!(data.rows[0].user_id === req.token.user_id)) return res.sendStatus(403);
         
         // Get Night info if can't get defaults to false so the code correspondent to that part is ignored
         const movie_id = req.body.movie_id || false;
--- src/nights/queries.js
+++ src/nights/queries.js
@@ -7,8 +7,10 @@ const updateDate = "UPDATE movie_nights SET movie_night_date = $1 WHERE movie_ni
 const updateDescription = "UPDATE movie_nights SET description = $1 WHERE movie_night_id = $2";
 
 // user_night table queries
+const getUserNight = "SELECT * FROM user_nights WHERE (user_id, movie_night_id) = ($1, $2)";
 const getNightByUserID = "SELECT user_nights.user_id, user_nights.movie_night_id, movie_id, movie_night_date, user_nights.confirmed, description FROM user_nights INNER JOIN movie_nights ON movie_nights.movie_night_id = user_nights.movie_night_id WHERE user_id = $1";
 const removeNightsUser = "DELETE FROM user_nights WHERE user_id = $1";
+const addUserNight = "INSERT INTO user_nights (user_id, movie_night_id, confirm) VALUES ($1, $2, $3)";
 
 module.exports = {
     getNightByID,
@@ -17,6 +19,8 @@ module.exports = {
     updateMovie,
     updateDate,
     updateDescription,
+    getUserNight,
     getNightByUserID,
     removeNightsUser,
+    addUserNight,
 };
