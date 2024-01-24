ef438d7a5d95a9f84dd6a61d3dda399af2813965
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-01-24 14:53:10 +0000

    integration with TMDB API (inicial fase)

---
+++ public/js/movies.js
@@ -0,0 +1,22 @@
+const homePopular = document.getElementById('homePopular');
+const homeTop = document.getElementById('homeTop');
+const homeUpcoming = document.getElementById('homeUpcoming');
+
+const movieSection = document.getElementById('movies');
+
+async function getMovies(type, page) {
+    await fetch(`/api/movies/${type}/${page}`, {
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${accessToken}` }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        fillPage(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function fillPage(data) {
+    
+}
